915672dfa8f20a78196fd06826a1dfc1b6f19cb2
acuasami <81541362+acuasami@users.noreply.github.com>
2025-12-06 11:07:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,15 @@ import hmac
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'una_clave_secreta_muy_fuerte_aqui') # Cambiar por una clave más segura
 
-# Usar la URI de tu cuaderno de railway.ipynb
-DB_URI = 'postgresql://postgres:KAGJhRklTcsevGqKEgCNPfmdDiGzsLyQ@switchyard.proxy.rlwy.net:13155/railway' #
+uri = 'postgresql://postgres:KAGJhRklTcsevGqKEgCNPfmdDiGzsLyQ@switchyard.proxy.rlwy.net:13155/railway'
+result = urlparse(uri)
+DB_URI= {
+    'user': result.username,
+    'password': result.password,
+    'host': result.hostname,
+    'port': result.port,
+    'dbname': result.path.lstrip('/')
+}
 
 # Función para obtener la conexión a la DB
 def get_db_connection():
@@ -303,3 +310,4 @@ if __name__ == '__main__':
     # Usar un puerto dinámico en Railway
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
